185af286827ccd662bcba98d3537b7d3c2a51785
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2026-05-08 15:57:12 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> hash;
+        for (int n: nums) {
+            if (hash.contains(n)) return true;
+            hash.insert(n);
+        }
+        return false;
+    }
+};
