5f6d7d37f6c7a90fd2ee52f00487f52e8a8119d4
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2026-05-14 16:08:15 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+public:
+    stack<int> stk;
+    stack<int> min_stk;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        stk.push(val);
+        if (!min_stk.empty()) 
+            min_stk.push(min(min_stk.top(), val));
+        else 
+            min_stk.push(val);
+    }
+    
+    void pop() {
+        stk.pop();
+        min_stk.pop();
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return min_stk.top();
+    }
+};
