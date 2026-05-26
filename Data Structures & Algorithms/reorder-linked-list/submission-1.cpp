dc198c75f291efa157309ab256843dd165953fd0
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2026-05-26 17:55:55 +0530

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        // Split
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        ListNode* secondHalf = slow->next;
+        slow->next = nullptr;
+        
+        // Reverse second half
+        ListNode* prev = nullptr;
+        ListNode* curr = secondHalf;
+        while (curr) {
+            ListNode* tmp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        // prev is the second half's reversed head
+        ListNode* ptr1 = head; // list 1 (first half)
+        ListNode* ptr2 = prev; // list 2 (reversed second half)
+
+        // merge two lists
+        ListNode *tmp1 = nullptr, *tmp2 = nullptr;
+        while (ptr1 && ptr2) {
+            tmp1 = ptr1->next;
+            ptr1->next = ptr2;
+            ptr1 = ptr1->next;
+            ptr2 = tmp1;
+        }
+    }
+};
