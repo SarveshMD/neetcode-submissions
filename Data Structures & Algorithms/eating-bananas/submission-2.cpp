9d02fcf70eaf5e3007acaf44fb77419434660ecf
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2026-08-01 13:26:46 +0100

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValidK(vector<int>& piles, int k, int h) {
+        int hours = 0;
+        for (int pile: piles) {
+            hours += pile / k;
+            if (pile%k != 0) hours++;
+        }
+        return hours <= h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+
+        int bestValidK = right;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (isValidK(piles, mid, h)) {
+                bestValidK = min(right, mid);
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return bestValidK;
+    }
+};
